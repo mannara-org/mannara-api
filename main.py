ca5d73f844fdbe185dcc3adde61d088e5a3682f2
Azsu-rae <aitameurmedilyas@gmail.com>
2026-09-10 17:06:10 +0100

    CORS setup for WAIW

--- main.py
+++ main.py
@@ -2,8 +2,18 @@
 import json
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
